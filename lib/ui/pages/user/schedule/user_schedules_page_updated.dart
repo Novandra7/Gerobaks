adbf83c2c07e2cb632bd83577ecc978ad1afe46a
Novandra7 <awangarissaputra@gmail.com>
2026-04-25 13:30:33 +0800

    style(user_schedules): format user schedules page

--- lib/ui/pages/user/schedule/user_schedules_page_updated.dart
+++ lib/ui/pages/user/schedule/user_schedules_page_updated.dart
@@ -193,7 +193,7 @@ class _UserSchedulesPageNewState extends State<UserSchedulesPageNew> {
         borderRadius: BorderRadius.circular(16),
         boxShadow: [
           BoxShadow(
-            color: Colors.black.withValues(alpha: 0.05),
+            color: Colors.black.withOpacity(0.05),
             blurRadius: 10,
             offset: const Offset(0, 5),
           ),
